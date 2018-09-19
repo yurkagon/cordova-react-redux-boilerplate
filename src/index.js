7edb0bb1aa5afe54cc11cca18811d40a21d8c036
yurkagon <owngameplay@gmail.com>
2018-09-19 19:49:10 +0300

    [ADD] cordova, android and ios platforms

--- src/index.js
+++ src/index.js
@@ -4,5 +4,30 @@ import App from './bootstrap';
 import registerServiceWorker from './bootstrap/registerServiceWorker';
 import './style/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// specific configuration for different platforms
+const configureApp = {
+  android: () => {
+    console.log('The app runs on Android');
+  },
+  ios: () => {
+    console.log('The app runs on IOS');
+  },
+  browser: () => {
+    console.log('The app runs in Browser');
+  },
+};
+
+const startApp = () => {
+  const { platformId } = window.cordova;
+  configureApp[platformId]();
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+if (window.cordova) {
+  document.addEventListener('deviceready', startApp, false);
+} else {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  registerServiceWorker();
+  configureApp['browser']();
+}
+
