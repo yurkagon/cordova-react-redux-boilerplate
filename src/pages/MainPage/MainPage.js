1bc481ceaa6c369830af81e085e10a15f703bc6a
yurkagon <owngameplay@gmail.com>
2018-09-19 23:36:48 +0300

    [ADD] PropTypes

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import logo from '../../static/svg/logo.svg';
 
 import { setName } from '../../actions/app';
 
-class App extends Component {
-  render() {
-    const { name, setAppName } = this.props;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {name}
-        </p>
-        <button type="button" onClick={() => setAppName('REACT REDUX CORDOVA BOILERPLATE')}>
-          SHOW NAME
-        </button>
-      </div>
-    );
-  }
-}
+const App = ({ name, setText }) => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      {name}
+    </p>
+    <button type="button" onClick={() => setText('REACT REDUX CORDOVA BOILERPLATE')}>
+      SHOW NAME
+    </button>
+  </div>
+);
+
+App.propTypes = {
+  name: PropTypes.string.isRequired,
+  setText: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = state => ({
   name: state.app.name,
 });
 
 const mapDispatchToProps = dispatch => ({
-  setAppName: bindActionCreators(setName, dispatch),
+  setText: bindActionCreators(setName, dispatch),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
